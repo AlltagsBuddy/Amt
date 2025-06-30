25fd9de4538aac0a4f01600174af2ef415f8ad40
AlltagsBuddy <info@alltags-buddy.de>
2025-06-30 19:58:00 +0200

    app.py

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ from math import radians, cos, sin, asin, sqrt
 
 app = Flask(__name__)
 
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello from AlltagsBuddy Amt!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
 
 import os
 
